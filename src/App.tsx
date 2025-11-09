072d57bb7d9e0c4a618f023e4fd49f145d4eb784
Mikael <anz.mikail@mail.ru>
2025-11-09 16:02:01 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import {useState} from "react";
 import usePWAInstall from './usePWAInstall';
 import './App.css'
 
 
 function App() {
     const { installPrompt, handleInstallClick } = usePWAInstall();
+    // @ts-ignore
+    const [counter, setCounter] = useState<number>(0);
+    const clickUp = () => {
+        setCounter(counter+1)
+    }
+    const clickDown = () => {
+        setCounter(counter-1)
+    }
     return (
         <div>
             {installPrompt && (
@@ -11,7 +20,10 @@ function App() {
                     Установить приложение
                 </button>
             )}
-            <h1>PWA приложение test33</h1>
+            <h1>PWA приложение test44</h1>
+            <button onClick={clickUp}>+</button>
+            {counter}
+            <button onClick={clickDown}>-</button>
         </div>
     )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,46 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import {type ManifestOptions, VitePWA} from "vite-plugin-pwa";
+import {type ManifestOptions, VitePWA} from "vite-plugin-pwa";
 
 
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 
 
-// const manifest: Partial<ManifestOptions> | false = {
-//   "orientation":"any",
-//   "display":"standalone",
-//   "start_url": "/index.html",
-//   "lang":"ru-RU",
-//   "name":"Fitness App",
-//   "theme_color":"#10100f",
-//   "background_color":"#85e8c2",
-//   "icons":[
-//     {
-//       "purpose":"maskable",
-//       "sizes":"512x512",
-//       "src":"icon512_maskable.png",
-//       "type":"image/png"
-//     },
-//     {
-//       "purpose":"any",
-//       "sizes":"512x512",
-//       "src":"icon512_rounded.png",
-//       "type":"image/png"
-//     }],
-//   screenshots: [
-//     {
-//       src: "/screenshots/desktop.png",
-//       type: "image/png",
-//       sizes: "1913x895",
-//       form_factor: "wide",
-//     },
-//     {
-//       src: "/screenshots/mobile.png",
-//       type: "image/png",
-//       sizes: "401x794",
-//       form_factor: 'narrow',
-//     },
-//   ],};
+const manifest: Partial<ManifestOptions> | false = {
+  "orientation":"any",
+  "display":"standalone",
+  "start_url": "/index.html",
+  "lang":"ru-RU",
+  "name":"Fitness App",
+  "theme_color":"#10100f",
+  "background_color":"#85e8c2",
+  "icons":[
+    {
+      "purpose":"maskable",
+      "sizes":"512x512",
+      "src":"icon512_maskable.png",
+      "type":"image/png"
+    },
+    {
+      "purpose":"any",
+      "sizes":"512x512",
+      "src":"icon512_rounded.png",
+      "type":"image/png"
+    }],
+  screenshots: [
+    {
+      src: "/screenshots/desktop.png",
+      type: "image/png",
+      sizes: "1913x895",
+      form_factor: "wide",
+    },
+    {
+      src: "/screenshots/mobile.png",
+      type: "image/png",
+      sizes: "401x794",
+      form_factor: 'narrow',
+    },
+  ],};
 
 export default defineConfig({
   plugins: [
@@ -48,7 +48,7 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       workbox: {globPatterns: ["**/*.{js,ts,tsx, html, css, ico, png, svg}"],},
-      // manifest: manifest,
+      manifest: manifest,
     })
   ],
 })
