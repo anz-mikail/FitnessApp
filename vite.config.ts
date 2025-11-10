11a59265db0b265134a788b424693286e8281c43
Mikael <anz.mikail@mail.ru>
2025-11-10 11:42:07 +0300

    test branch

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import {type ManifestOptions } from "vite-plugin-pwa";
+import {type ManifestOptions } from "vite-plugin-pwa";
 import { VitePWA } from "vite-plugin-pwa";
 
 
-// const manifest: Partial<ManifestOptions> | false = {
-//   "orientation":"any",
-//   "display":"standalone",
-//   "start_url": "/index.html",
-//   "lang":"ru-RU",
-//   "name":"Fitness App",
-//   "theme_color":"#10100f",
-//   "background_color":"#85e8c2",
-//   "icons":[
-//     {
-//       "purpose":"maskable",
-//       "sizes":"512x512",
-//       "src":"icon512_maskable.png",
-//       "type":"image/png"
-//     },
-//     {
-//       "purpose":"any",
-//       "sizes":"512x512",
-//       "src":"icon512_rounded.png",
-//       "type":"image/png"
-//     }],
-//   screenshots: [
-//     {
-//       src: "/screenshots/desktop.png",
-//       type: "image/png",
-//       sizes: "1913x895",
-//       form_factor: "wide",
-//     },
-//     {
-//       src: "/screenshots/mobile.png",
-//       type: "image/png",
-//       sizes: "401x794",
-//       form_factor: 'narrow',
-//     },
-//   ],};
+const manifest: Partial<ManifestOptions> | false = {
+  "theme_color":"#19191a",
+    "background_color":"#7fb1c3",
+    "icons":[{
+        "purpose":"maskable",
+        "sizes":"512x512",
+        "src":"icon512_maskable.png",
+        "type":"image/png"
+    },{
+        "purpose":"any",
+        "sizes":"512x512",
+        "src":"icon512_rounded.png",
+        "type":"image/png"
+    }],
+      "orientation":"any",
+      "display":"fullscreen",
+      "lang":"ru-RU",
+      "name":"Fitness grozny",
+      "start_url":"/FitnessApp/index.html",
+      "scope":"https://anz-mikail.github.io"
+  }
 
 export default defineConfig({
   plugins: [
@@ -46,7 +32,7 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       workbox: {globPatterns: ["**/*.{js,ts,tsx, html, css, ico, png, svg}"],},
-      // manifest: manifest,
+      manifest: manifest,
     })
   ],
   base: "/FitnessApp"
